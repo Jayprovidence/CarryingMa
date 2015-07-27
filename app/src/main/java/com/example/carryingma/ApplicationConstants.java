c0a9423c8831dae317725759e7d34619e82c6b96
ivan8ivan8 <ivan8ivan8@gmail.com>
2015-07-27 16:54:41 +0800

    Create Java

---
+++ app/src/main/java/com/example/carryingma/ApplicationConstants.java
@@ -0,0 +1,13 @@
+package com.example.carryingma;
+
+public interface ApplicationConstants {
+
+    // Php Application URL to store Reg ID created
+    static final String APP_SERVER_URL = "http://10.0.36.116/gcm/gcm.php?shareRegId=true";
+
+    // Google Project Number
+    static final String GOOGLE_PROJ_ID = "830181069604";
+    // Message Key
+    static final String MSG_KEY = "m";
+
+}
