58c8bfe9046e259a492f26ff927135ef2cbd5ab7
ivan8ivan8 <ivan8ivan8@gmail.com>
2015-08-25 16:57:34 +0800

    Add Google login function

--- app/src/main/java/com/example/carryingma/MainActivity.java
+++ app/src/main/java/com/example/carryingma/MainActivity.java
@@ -4,6 +4,7 @@ package com.example.carryingma;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentSender;
@@ -30,45 +31,56 @@ import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
 
 import java.io.IOException;
 
 import static com.example.carryingma.GCM.RegistrationIntentService.getDefaults;
 
 
-public class MainActivity extends Activity  implements View.OnClickListener/*implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener*/{
+public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
 
+    //google login stuff
     /* RequestCode for resolutions involving sign-in */
     private static final int RC_SIGN_IN = 9001;
-
+    /* Keys for persisting instance variables in savedInstanceState */
+    private static final String KEY_IS_RESOLVING = "is_resolving";
+    private static final String KEY_SHOULD_RESOLVE = "should_resolve";
+    /* View to display current status (signed-in, signed-out, disconnected, etc) */
+    private TextView mStatus, personImageUrl, personProfileUrl, personEmail;
+    /* Is there a ConnectionResult resolution in progress? */
+    private boolean mIsResolving = false;
+    /* Should we automatically resolve ConnectionResults when possible? */
+    private boolean mShouldResolve = false;
     /* Client for accessing Google APIs */
     private GoogleApiClient mGoogleApiClient;
 
-    /* Is there a ConnectionResult resolution in progress? */
-    private boolean mIsResolving = false;
-
-    /* Should we automatically resolve ConnectionResults when possible? */
-    private boolean mShouldResolve = false;
-
+    //gcm stuff
     private BroadcastReceiver mRegistrationBroadcastReceiver;
     private ProgressBar mRegistrationProgressBar;
     private TextView mInformationTextView;
-
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-    private static final String TAG = "GcmActivity";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       /* // Build GoogleApiClient with access to basic profile
+        // Build GoogleApiClient with access to basic profile
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(Plus.API)
                 .addScope(new Scope(Scopes.PROFILE))
-                .build();*/
+                .build();
+
+        // Restore from saved instance state
+        // [START restore_saved_instance_state]
+        if (savedInstanceState != null) {
+            mIsResolving = savedInstanceState.getBoolean(KEY_IS_RESOLVING);
+            mShouldResolve = savedInstanceState.getBoolean(KEY_SHOULD_RESOLVE);
+        }
 
         //button click listeners
         findViewById(R.id.httppost_button).setOnClickListener(this);
@@ -78,7 +90,17 @@ public class MainActivity extends Activity  implements View.OnClickListener/*imp
         findViewById(R.id.userexam_button).setOnClickListener(this);
         findViewById(R.id.gcm_button).setOnClickListener(this);
         findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        findViewById(R.id.disconnect_button).setOnClickListener(this);
 
+        // Start with sign-in button disabled until sign-in either succeeds or fails
+        findViewById(R.id.sign_in_button).setEnabled(false);
+
+        // Set up view instances
+        mStatus = (TextView) findViewById(R.id.status);
+        personImageUrl = (TextView) findViewById(R.id.person_photo_url);
+        personProfileUrl = (TextView) findViewById(R.id.person_profile_url);
+        personEmail = (TextView) findViewById(R.id.person_email);
 
         //display progress bar for registering gcm instance id and token's progress status
         mRegistrationProgressBar = (ProgressBar) findViewById(R.id.registrationProgressBar);
@@ -108,6 +130,7 @@ public class MainActivity extends Activity  implements View.OnClickListener/*imp
 
         //display current user
         String user = getDefaults(getString(R.string.current_user), MainActivity.this);
+        user = "User: "+ user;
         Toast.makeText(MainActivity.this, user, Toast.LENGTH_LONG).show();
     }
 
@@ -157,28 +180,89 @@ public class MainActivity extends Activity  implements View.OnClickListener/*imp
                 intent = new Intent(this, GcmActivity.class);
                 startActivity(intent);
                 break;
-            /*case R.id.sign_in_button:
+            case R.id.sign_in_button:
                 // User clicked the sign-in button, so begin the sign-in process and automatically
                 // attempt to resolve any errors that occur.
+                mStatus.setText(R.string.signing_in);
                 // [START sign_in_clicked]
                 mShouldResolve = true;
                 mGoogleApiClient.connect();
-                // Show a message to the user that we are signing in.
-                mStatusTextView.setText(R.string.signing_in);
                 // [END sign_in_clicked]
-                break;*/
+                break;
+            case R.id.sign_out_button:
+                // Clear the default account so that GoogleApiClient will not automatically
+                // connect in the future.
+                // [START sign_out_clicked]
+                if (mGoogleApiClient.isConnected()) {
+                    Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+                    mGoogleApiClient.disconnect();
+                }
+                // [END sign_out_clicked]
+                updateUI(false);
+                break;
+            case R.id.disconnect_button:
+                // Revoke all granted permissions and clear the default account.  The user will have
+                // to pass the consent screen to sign in again.
+                // [START disconnect_clicked]
+                if (mGoogleApiClient.isConnected()) {
+                    Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+                    Plus.AccountApi.revokeAccessAndDisconnect(mGoogleApiClient);
+                    mGoogleApiClient.disconnect();
+                }
+                // [END disconnect_clicked]
+                updateUI(false);
+                break;
 
         }
     }
 
-    /*//reset state of flags
+    //[START google_sign_in_UI]
+    private void updateUI(boolean isSignedIn) {
+        if (isSignedIn) {
+            // Show signed-in user's name
+            Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+            String name = currentPerson.getDisplayName();
+            String photo = currentPerson.getImage().getUrl();
+            String profile = currentPerson.getUrl();
+            String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+
+            mStatus.setText(getString(R.string.signed_in_fmt, name));
+            personImageUrl.setText(getString(R.string.person_photo_url_fmt, photo));
+            personProfileUrl.setText(getString(R.string.person_profile_url_fmt, profile));
+            personEmail.setText(getString(R.string.person_email, email));
+
+            // Set button visibility
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+            // Show signed-out message
+            mStatus.setText(R.string.signed_out);
+
+            // Set button visibility
+            findViewById(R.id.sign_in_button).setEnabled(true);
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
+    }
+    //[END google_sign_in_UI]
+
+    // [START on_save_instance_state]
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_IS_RESOLVING, mIsResolving);
+        outState.putBoolean(KEY_SHOULD_RESOLVE, mShouldResolve);
+    }
+    // [END on_save_instance_state]
+
+    // [START on_activity_result]
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Log.d(TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
 
         if (requestCode == RC_SIGN_IN) {
-            // If the error resolution was not successful we should not resolve further.
+            // If the error resolution was not successful we should not resolve further errors.
             if (resultCode != RESULT_OK) {
                 mShouldResolve = false;
             }
@@ -187,19 +271,28 @@ public class MainActivity extends Activity  implements View.OnClickListener/*imp
             mGoogleApiClient.connect();
         }
     }
-    //successful signed in,
+    // [END on_activity_result]
+
     @Override
     public void onConnected(Bundle bundle) {
         // onConnected indicates that an account was selected on the device, that the selected
         // account has granted any requested permissions to our app and that we were able to
         // establish a service connection to Google Play services.
         Log.d(TAG, "onConnected:" + bundle);
-        mShouldResolve = false;
 
         // Show the signed-in UI
-        showSignedInUI();
+        updateUI(true);
     }
-    //connect to google login service
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // The connection to Google Play services was lost. The GoogleApiClient will automatically
+        // attempt to re-connect. Any UI elements that depend on connection to Google APIs should
+        // be hidden or disabled until onConnected is called again.
+        Log.w(TAG, "onConnectionSuspended:" + i);
+    }
+
+    // [START on_connection_failed]
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         // Could not connect to Google Play Services.  The user needs to select an account,
@@ -224,11 +317,36 @@ public class MainActivity extends Activity  implements View.OnClickListener/*imp
             }
         } else {
             // Show the signed-out UI
-            showSignedOutUI();
+            updateUI(false);
         }
-    }*/
+    }
+    // [END on_connection_failed]
 
-    //Activity lifecycle
+    private void showErrorDialog(ConnectionResult connectionResult) {
+        int errorCode = connectionResult.getErrorCode();
+
+        if (GooglePlayServicesUtil.isUserRecoverableError(errorCode)) {
+            // Show the default Google Play services error dialog which may still start an intent
+            // on our behalf if the user can resolve the issue.
+            GooglePlayServicesUtil.getErrorDialog(errorCode, this, RC_SIGN_IN,
+                    new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            mShouldResolve = false;
+                            updateUI(false);
+                        }
+                    }).show();
+        } else {
+            // No default Google Play Services error, display a message to the user.
+            String errorString = getString(R.string.play_services_error_fmt, errorCode);
+            Toast.makeText(this, errorString, Toast.LENGTH_SHORT).show();
+
+            mShouldResolve = false;
+            updateUI(false);
+        }
+    }
+
+    //[START Activity_Life_Cycle]
     @Override
     protected void onStart() {
         super.onStart();
@@ -241,25 +359,28 @@ public class MainActivity extends Activity  implements View.OnClickListener/*imp
             startActivity(intent);
         }
 
-        //mGoogleApiClient.connect();
+        mGoogleApiClient.connect();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        //mGoogleApiClient.disconnect();
+        mGoogleApiClient.disconnect();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        //check-register gcm
         LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
                 new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
     }
 
     @Override
     protected void onPause() {
+        //unregister gcm
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
         super.onPause();
     }
+    //[END Activity_Life_Cycle]
 }
